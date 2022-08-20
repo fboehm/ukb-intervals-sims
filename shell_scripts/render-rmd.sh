73ef650d45224567f105a575a19672698f5ede67
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-08-20 14:31:03 -0400

    added shell script to render Rmd

---
+++ shell_scripts/render-rmd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --partition=mulan,main
+#SBATCH --time=3-00:00:00
+#SBATCH --job-name=cvplus
+#SBATCH --mem=16G
+#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/render-rmd_%j_%a.out
+#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/render-rmd_%j_%a.err
+
+Rscript -e 'rmarkdown::render(input = "~/research/ukb-intervals-sims/Rmd/cv-plus-simulations.Rmd",
+                              output_file = "~/research/ukb-intervals-sims/Rmd/cv-plus-simulations-alpha0.1-hsq0.1-pcausal0.1.html",
+                              params = list(hsq=0.1, pcausal=0.1))'
