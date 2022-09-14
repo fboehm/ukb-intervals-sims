e7b0f3ca4bfb148482975be65dc2dfaeaf4a33e3
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-09-14 18:50:18 -0400

    added shell script for plink subsetting

---
+++ shell_scripts/subset_plink_files_for_simulating_traits.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --partition=mulan,main
+#SBATCH --time=1-00:00:00
+#SBATCH --job-name=plink_subset_SNPs
+#SBATCH --mem=64G
+#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals-sims/cluster_outputs/subset_snps.out
+#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals-sims/cluster_outputs/subset_snps.err
+
+# GOAL: Create a set of plink files for Chr1 for use in simulating traits with GCTA
+dir=~/research/ukb-intervals-sims/hapmap3/hm3_ukb/
+subject_list=~/research/ukb-intervals-sims/hapmap3/subjects_for_sims.txt
+outdir=~/research/ukb-intervals-sims/hapmap3/plink_files_for_sims/
+
+mkdir -p ${outdir}
+
+for chr in 1; do
+    plink --bfile ${dir}chr${chr} --keep ${subject_list} --make-bed --allow-no-sex --out ${outdir}chr${chr}
+  done
