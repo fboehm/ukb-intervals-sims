773d03ab203a1f509dde7ac87b3506cd612b772a
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-09-14 18:39:48 -0400

    whitesp deleted

--- shell_scripts/subset_plink_files_to_HM3_SNPs.sh
+++ shell_scripts/subset_plink_files_to_HM3_SNPs.sh
@@ -4,7 +4,6 @@
 #SBATCH --time=1-00:00:00
 #SBATCH --job-name=plink_subset_SNPs
 #SBATCH --mem=64G
-
 #SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals-sims/cluster_outputs/subset_snps.out
 #SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals-sims/cluster_outputs/subset_snps.err
 
