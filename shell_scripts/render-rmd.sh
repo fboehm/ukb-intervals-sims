7247c0c81ef2b878d9d94681746ebd31f42af23a
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-09-26 10:41:10 -0400

    submitted render rmd jobs

--- shell_scripts/render-rmd.sh
+++ shell_scripts/render-rmd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --partition=mulan,main
-#SBATCH --time=3-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --job-name=cvplus
 #SBATCH --mem=16G
 #SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals-sims/cluster_outputs/render-rmd_%j_%a.out
 #SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals-sims/cluster_outputs/render-rmd_%j_%a.err
 
-Rscript -e 'rmarkdown::render(input = "~/research/ukb-intervals-sims/Rmd/cv-plus-simulations.Rmd",
-                              output_file = "~/research/ukb-intervals-sims/results/DBSLMM/cv-plus-simulations-alpha0.1.html"
+Rscript -e 'rmarkdown::render(input = "~/research/ukb-intervals-sims/Rmd/CT-cv-plus-simulations.Rmd",
+                              output_file = "~/research/ukb-intervals-sims/results/CT/CT-cv-plus-simulations-alpha0.1.html"
                              )'
